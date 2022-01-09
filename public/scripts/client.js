1ea840e8ea660ae5dd17d6ec742754d6870b4cf0
Hollie Hensman <hensmanh@gmail.com>
2022-01-09 01:37:36 +0000

    Refomat to include Responsive Design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,85 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
+const createTweetElement = function (tweet) {
+  console.log(tweet.created_at);
+  const created_at = timeago.format(tweet.created_at);
+  const $tweetElement = `
+  <article class="tweet">
+  <div class="tweet-header">
+    <div class="profile">
+      <img src=${tweet.user.avatars}>
+      <p>${tweet.user.name}</p>
+    </div>
+
+    <div>
+      <p class="tweet-handle">${tweet.user.handle}</p>
+
+    </div>
+  </div>
+
+  <div class="tweet-body">
+    <p>${escape(tweet.content.text)}</p>
+    <div class="tweet-body-border">
+
+    </div>
+  </div>
+  <div class="tweet-footer">
+    <div>${created_at}</div>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+
+  </div>
+</article>`;
+
+  return $tweetElement;
+};
+
+const renderTweets = function (tweets) {
+  console.log("Tweets", tweets);
+  tweets.forEach((element) => {
+    $(".tweet-container").append(createTweetElement(element));
+  });
+};
+
+$(document).ready(() => {
+  loadTweets();
+
+  $("form").submit((event) => {
+    console.log($("form").serialize());
+    event.preventDefault();
+    console.log($("#tweet-text").val().length);
+    if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
+      $(".err-msg").slideDown();
+      $(".err-msg").html("<span>** No Text Provided! **</span>");
+    }
+    if ($("#tweet-text").val().length > 140) {
+      $(".err-msg").slideDown();
+      $(".err-msg").html("<span>Sorry! Characters Exceed 140");
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: $("form").serialize(),
+      success: function () {
+        $("#tweet-text").val("");
+        console.log("It Tweeted!");
+        loadTweets();
+      },
+    });
+  });
+
+  function loadTweets() {
+    $.get("/tweets", function (data, status) {
+      console.log(data);
+      renderTweets(data.reverse());
+    });
+  }
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $("#tweet-text").on("keyup", () => {
+    const tweetElement = $("#tweet-text");
+    const tweetText = tweetElement.val();
+
+    if (tweetText.length > 140) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black");
+    }
+
+    $(".counter").val(140 - tweetText.length);
+  });
+});
